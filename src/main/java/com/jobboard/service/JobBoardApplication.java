b1f6333d4fae49719983ea543379203cba8e18e9
Vy Nguyen <alexisleon0611@gmail.com>
2024-11-03 07:49:12 -0600

    setup java backend w/ connection to db

---
+++ src/main/java/com/jobboard/service/JobBoardApplication.java
@@ -0,0 +1,16 @@
+package com.jobboard.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBoardApplication.class, args);
+
+
+		System.out.println("Hello World");
+	}
+
+}
