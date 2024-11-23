2e93d85dd571b258c7d8e027acbdbb3db9acc7df
Vy Nguyen <alexisleon0611@gmail.com>
2024-11-23 13:11:40 -0600

    stub out the add-job page

--- job-board-ui/src/index.js
+++ job-board-ui/src/index.js
@@ -17,7 +17,6 @@ const App = () => {
             {" "}
             Home
           </NavLink>
-
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className={`nav-item active`}>
--- job-board-ui/src/page/add-job.js
+++ job-board-ui/src/page/add-job.js
@@ -1,20 +1,49 @@
+import { useCallback, useRef } from "react";
+import { addJob } from "../data/jobs";
+import { useNavigate } from "react-router";
+
 export const AddJob = () => {
+  const navigate = useNavigate();
+  const jobTitle = useRef();
+  const hourlyRate = useRef();
+  const jobDescription = useRef();
+
+  const onSubmit = useCallback(async () => {
+    const response = await addJob({
+      jobTitle: jobTitle.current.value,
+      hourlyRate: Number(hourlyRate.current.value),
+      jobDescription: jobDescription.current.value,
+    });
+
+    navigate("/");
+  }, [jobTitle, hourlyRate, jobDescription]);
+
   return (
     <div className="container">
       <form>
         <div className="form-group">
           <label>Job Title</label>
-          <input className="form-control" placeholder="Enter Job Title" />
+          <input
+            ref={jobTitle}
+            className="form-control"
+            placeholder="Enter Job Title"
+          />
         </div>
 
         <div className="form-group">
           <label>Hourly Rate</label>
-          <input type="number" className="form-control" placeholder="Hourly Rate" />
+          <input
+            ref={hourlyRate}
+            type="number"
+            className="form-control"
+            placeholder="Hourly Rate"
+          />
         </div>
 
         <div className="form-group">
           <label for="exampleFormControlTextarea1">Job Description</label>
           <textarea
+            ref={jobDescription}
             className="form-control"
             id="exampleFormControlTextarea1"
             rows="3"
@@ -22,6 +51,7 @@ export const AddJob = () => {
         </div>
 
         <button
+          onClick={onSubmit}
           type="button"
           style={{ marginTop: "10px" }}
           className="btn btn-primary"
