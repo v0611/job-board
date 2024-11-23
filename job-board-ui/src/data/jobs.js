d2056f69c0ba6f84f2124a866e7fc6f9cc6e459e
Vy Nguyen <alexisleon0611@gmail.com>
2024-11-23 13:05:26 -0600

    add data stubs

---
+++ job-board-ui/src/data/jobs.js
@@ -0,0 +1,6 @@
+export const getJobs = () => {};
+export const getJob = (jobId) => {};
+export const addJob = (payload) => {};
+export const editJob = (jobId, payload) => {};
+export const deleteJob = (jobId) => {};
+export const applyForJob = (jobId, payload) => {};
